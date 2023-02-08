c69c9521aee0ce1a0648f9859930b6b207d1c16c
victor <vicdotexe@gmail.com>
2023-02-08 13:56:55 -0800

    Main tasks completed and implemented ICollection<T>

---
+++ GenericsHomework/GenericsHomework.Tests/TestNode.cs
@@ -0,0 +1,151 @@
+namespace GenericsHomework.Tests
+{
+    [TestClass]
+    public class TestNode
+    {
+
+        [TestMethod]
+        public void Node_ExistsAmongstMany_False()
+        {
+            var node = new Node<int>(10);
+            node.Append(11).Append(12).Append(13);
+            Assert.IsFalse(node.Exists(14));
+        }
+
+        [TestMethod]
+        public void Node_ExistsAmongstMany_True()
+        {
+            var node = new Node<int>(10);
+            node.Append(11).Append(12).Append(13);
+            Assert.IsTrue(node.Exists(12));
+        }
+
+        [TestMethod]
+        public void Node_ExistsInSingular_True()
+        {
+            var node = new Node<int>(11);
+            Assert.IsTrue(node.Exists(11));
+        }
+
+        [TestMethod]
+        public void Node_OnlySelf_NextEqualsSelf()
+        {
+            var node = new Node<int>(5);
+            Assert.AreEqual(node, node.Next);
+        }
+
+        [TestMethod]
+        public void Node_Clears()
+        {
+            var node0 = new Node<int>(0);
+            var node1 = node0.Append(1);
+            var node2 = node1.Append(2);
+            var node3 = node2.Append(3);
+            Assert.IsTrue(node1.Exists(2));
+            node0.Clear();
+            Assert.IsFalse(node1.Exists(2));
+            Assert.AreEqual(node1, node1.Next);
+        }
+
+        [TestMethod]
+        public void Node_NotNullValue_ToString()
+        {
+            var node = new Node<int>(7);
+            Assert.AreEqual(node.ToString(), "7");
+        }
+
+        [TestMethod]
+        public void Node_NullValue_ToString()
+        {
+            var node = new Node<object>(null!);
+            Assert.AreEqual(node.ToString(), null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Node_AppendDuplicate_ThrowsException()
+        {
+            var node = new Node<int>(0);
+            node.Append(1).Append(2).Append(0);
+        }
+
+        //ICollection
+
+        [TestMethod]
+        public void Node_Count()
+        {
+            var node = new Node<int>(0);
+            node.Append(1).Append(2).Append(3).Append(4);
+            Assert.AreEqual(node.Count, 5);
+        }
+
+        [TestMethod]
+        public void Node_Contains()
+        {
+            var node = new Node<int>(0);
+            node.Append(1).Append(2).Append(3).Append(4);
+
+            Assert.IsTrue(node.Contains(3));
+        }
+
+        [TestMethod]
+        public void Node_AddItem()
+        {
+            var node0 = new Node<int>(0);
+            var node1 = node0.Append(1);
+            var node2 = node1.Append(2);
+            var node3 = node2.Append(3);
+            var node4 = node3.Append(4);
+            node2.Add(8);
+            Assert.IsTrue(node0.Contains(8));
+            Assert.IsTrue(node4.Contains(8));
+        }
+
+        [TestMethod]
+        public void Node_RemoveValidItem()
+        {
+            var node = new Node<int>(0);
+            node.Append(1).Append(2).Append(3).Append(4);
+
+            Assert.IsTrue(node.Remove(3));
+        }
+
+        [TestMethod]
+        public void Node_RemoveInValidItem()
+        {
+            var node = new Node<int>(0);
+            node.Append(1).Append(2).Append(3).Append(4);
+
+            Assert.IsFalse(node.Remove(5));
+        }
+
+        [TestMethod]
+        public void Node_Enumerable()
+        {
+            var node = new Node<int>(1);
+            node.Append(2).Append(3).Append(4).Append(5);
+
+            var count = node.Sum();
+            Assert.AreEqual(15, count);
+        }
+
+        [TestMethod]
+        public void Node_CopyToArray()
+        {
+            var array = new int[20];
+
+            var node = new Node<int>(1);
+            node.Append(2).Append(3).Append(4).Append(5);
+
+            node.CopyTo(array, 15);
+            Assert.AreEqual(array.Sum(), node.Sum());
+        }
+
+        [TestMethod]
+        public void Node_IsReadonly_False()
+        {
+            var node = new Node<int>(1);
+            Assert.IsFalse(node.IsReadOnly);
+        }
+    }
+}
---
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace GenericsHomework
+{
+    public class Node<T> : ICollection<T>
+    {
+        public T Value { get; }
+
+        public Node<T> Next { get; private set; }
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = this;
+        }
+
+        public Node<T> Append(T value)
+        {
+            if (Exists(value))
+                throw new ArgumentException("Cannot add same value more than once", nameof(value));
+
+            var next = Next;
+            Next = new Node<T>(value)
+            {
+                Next = next
+            };
+            return Next;
+
+        }
+
+        public bool Exists(T value)
+        {
+            var current = this;
+            do
+            {
+                if (EqualityComparer<T>.Default.Equals(current.Value, value))
+                    return true;
+
+                current = current.Next;
+            } while (current != this);
+
+            return false;
+        }
+
+        public void Add(T item)
+        {
+            Append(item);
+        }
+
+        public void Clear()
+        {
+            if (Next == this)
+                return;
+            var next = Next;
+            Next = this;
+            next.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return Exists(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            var count = Count;
+            var current = this;
+            for (int i = arrayIndex; i < arrayIndex + count; i++)
+            {
+                array[i] = current.Value;
+                current = current.Next;
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            if (!Exists(item))
+                return false;
+            var current = this;
+            while (!EqualityComparer<T>.Default.Equals(current.Next.Value, item))
+            {
+                current = current.Next;
+            }
+
+            // get the node to remove
+            var toRemove = current.Next;
+            // set the node behind it's next to the removed node's next (bridge the gap that was created)
+            current.Next = toRemove.Next;
+            // apply a self-reference to the removed node
+            toRemove.Next = toRemove;
+
+            return true;
+        }
+
+        public int Count
+        {
+            get
+            {
+                var count = 1;
+                var current = this.Next;
+                while (current != this)
+                {
+                    count++;
+                    current = current.Next;
+                }
+
+                return count;
+            }
+        }
+
+        public bool IsReadOnly => false;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = this;
+            do
+            {
+                yield return current.Value;
+                current = current.Next;
+            } while(current != this );
+        }
+
+        public override string? ToString()
+        {
+            return Value?.ToString();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
